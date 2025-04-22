5ce047b392eb5e1177221f529d4f22ab8dd295ef
gasem <al.mazharish.g@phystech.edu>
2025-04-23 05:24:10 +0600

    functions and header implemented

---
+++ rgbw-i2c-driver.c
@@ -0,0 +1,82 @@
+#include "rgbw-i2c-driver.h"
+#include <time.h>
+
+i2chw_error_t rgbw_driver_init(i2chw_dev_t *dev, i2chw_bus_t bus)
+{
+  i2chw_cfg_t cfg = {
+      .bus_freq = I2CHW_400_KHZ,          // Because of min clock period = 2.5us
+      .dir_mode = I2CHW_MASTER_MODE};
+  i2chw_error_t error = I2CHW_Configure(bus, &cfg);
+  if (error != I2CHW_SUCCESS)
+    return error;
+
+  i2chw_error_t error = I2CHW_Init(bus);
+  if (error != I2CHW_SUCCESS)
+    return error;
+
+  dev->bus_num = bus;
+  dev->dev_addr = RGBW_DRIVER_ADDR;
+  dev->addr_width = I2CHW_ADDR_WIDTH_7BIT;
+
+  uint8_t reset_cfg[2] = {RESET_REG, 0b11100000};
+  i2chw_error_t error = I2CHW_WriteSync(dev, reset_cfg, sizeof(reset_cfg));
+  if (error != I2CHW_SUCCESS)
+    return error;
+
+  usleep(RESET_DELAY_MCS);
+
+  return I2CHW_SUCCESS;
+}
+
+i2chw_error_t rgbw_driver_set_channel_brightness(i2chw_dev_t *dev, uint8_t channel, uint8_t brightness)
+{
+  uint8_t reg_addr = IOUT_D1_REG + (channel - 1);
+  uint8_t data[2] = {reg_addr, brightness};
+  return I2CHW_WriteSync(dev, data, sizeof(data));
+}
+
+i2chw_error_t rgbw_driver_channel_switch(i2chw_dev_t *dev, uint8_t channel, bool state)
+{
+  // Reading current state of 4-th register
+  uint8_t enable_reg_state;
+  uint8_t data[2] = {ENABLE_REG};
+  i2chw_error_t error = I2CHW_WriteReadSync(&dev, data[0], sizeof(data[0]), &enable_reg_state, sizeof(enable_reg_state));
+  if (error != I2CHW_SUCCESS)
+    return error;
+
+  if (state)
+    enable_reg_state |= (1u << (channel - 1));
+  else
+    enable_reg_state &= ~(1u << (channel - 1));
+  data[1] = enable_reg_state;
+
+  return I2CHW_WriteSync(dev, data, sizeof(data));
+}
+
+i2chw_error_t rgbw_driver_all_channels_switch(i2chw_dev_t *dev, bool state)
+{
+  uint8_t data[2] = {ENABLE_REG, 0b00000000};
+  if (state)
+    data[1] = 0b01010101;
+
+  return I2CHW_WriteSync(dev, data, sizeof(data));
+}
+
+i2chw_error_t rgbw_driver_set_color(i2chw_dev_t *dev, uint8_t r, uint8_t g, uint8_t b, uint8_t w)
+{
+  uint8_t data[8] = {
+      IOUT_D1_REG, r,
+      IOUT_D2_REG, g,
+      IOUT_D3_REG, b,
+      IOUT_D4_REG, w};
+
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    uint8_t frame = {data[2 * i], data[2 * i + 1]};
+    i2chw_error_t error = I2CHW_WriteSync(dev, frame, sizeof(frame));
+    if (error != I2CHW_SUCCESS)
+      return error;
+  }
+
+  return I2CHW_SUCCESS;
+}
--- rgbw-i2c-driver.h
+++ rgbw-i2c-driver.h
@@ -1,6 +1,6 @@
 #include "i2chw.h"
 
-#define RGBW_DRIVER_ADD 0x30
+#define RGBW_DRIVER_ADDR 0x30
 
 #define RESET_REG 0x00
 #define ENABLE_REG 0x04
@@ -11,6 +11,75 @@
 
 #define RESET_DELAY_MCS 200
 
-i2chw_error_t rgbw_driver_init(){
-  
-}
+/** 
+ * @brief Initialize RGBW chip
+ * 
+ * @param dev - pointer to I2C device configuration
+ * @param bus_num - number of I2C bus
+ * @return hal_error_t - initialization result
+ *         I2CHW_SUCCESS - the chip was initialized successfully
+ *         I2CHW_ERR_INVALID_PARAMS - invalid parameters are given
+ *         I2CHW_ERR_HW - the chip was not initialized because of HW issues 
+ */
+i2chw_error_t rgbw_driver_init(i2chw_dev_t *dev, i2chw_bus_t bus);
+
+/** 
+ * @brief Set channel brightness
+ * 
+ * @param dev - pointer to I2C device configuration
+ * @param channel - number of channel
+ * @param brightness - value from 0 to 191, any higher value will reach Imax
+ * @return i2chw_error_t - result of transmission
+ *         I2CHW_SUCCESS - all data has been transmitted
+ *         I2CHW_ERR_INVALID_PARAMS - invalid parameters are given
+ *         I2CHW_ERR_TIMEOUT - transmission was not finished because of execution timeout
+ *         I2CHW_ERR_NACK - the device did't set ACK bit
+ *         I2CHW_ERR_HW - the bus has hardware issues
+ */
+i2chw_error_t rgbw_driver_set_channel_brightness(i2chw_dev_t *dev, uint8_t channel, uint8_t brightness);
+
+/** 
+ * @brief Turn the channel ON/OFF
+ * 
+ * @param dev - pointer to I2C device configuration
+ * @param channel - number of channel
+ * @param state - 1 - ON, 0 - OFF
+ * @return i2chw_error_t - result of transmission
+ *         I2CHW_SUCCESS - all data has been transmitted
+ *         I2CHW_ERR_INVALID_PARAMS - invalid parameters are given
+ *         I2CHW_ERR_TIMEOUT - transmission was not finished because of execution timeout
+ *         I2CHW_ERR_NACK - the device did't set ACK bit
+ *         I2CHW_ERR_HW - the bus has hardware issues
+ */
+i2chw_error_t rgbw_driver_channel_switch(i2chw_dev_t *dev, uint8_t channel, bool state);
+
+/** 
+ * @brief Turn all channels ON/OFF
+ * 
+ * @param dev - pointer to I2C device configuration
+ * @param state - 1 - ON, 0 - OFF
+ * @return i2chw_error_t - result of transmission
+ *         I2CHW_SUCCESS - all data has been transmitted
+ *         I2CHW_ERR_INVALID_PARAMS - invalid parameters are given
+ *         I2CHW_ERR_TIMEOUT - transmission was not finished because of execution timeout
+ *         I2CHW_ERR_NACK - the device did't set ACK bit
+ *         I2CHW_ERR_HW - the bus has hardware issues
+ */
+i2chw_error_t rgbw_driver_all_channels_switch(i2chw_dev_t *dev, bool state);
+
+/** 
+ * @brief Set the color in RGBW space
+ * 
+ * @param dev - pointer to I2C device configuration
+ * @param r - D1 channel value
+ * @param g - D2 channel value
+ * @param b - D3 channel value
+ * @param w - D4 channel value
+ * @return i2chw_error_t - result of transmission
+ *         I2CHW_SUCCESS - all data has been transmitted
+ *         I2CHW_ERR_INVALID_PARAMS - invalid parameters are given
+ *         I2CHW_ERR_TIMEOUT - transmission was not finished because of execution timeout
+ *         I2CHW_ERR_NACK - the device did't set ACK bit
+ *         I2CHW_ERR_HW - the bus has hardware issues
+ */
+i2chw_error_t rgbw_driver_set_color(i2chw_dev_t *dev, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
