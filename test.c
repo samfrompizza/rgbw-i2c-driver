284e7902b728f5bbbc7cc76fc7269344cc48480a
gasem <al.mazharish.g@phystech.edu>
2025-04-24 05:10:20 +0600

    test modified

--- test.c
+++ test.c
@@ -3,12 +3,30 @@
 int main() {
   i2chw_dev_t dev;
 
-  rgbw_driver_init(&dev, I2CHW_BUS_I2C0);
-  rgbw_driver_channel_switch(&dev, 2, 1);
-  rgbw_driver_set_channel_brightness(&dev, 2, 168);
-  rgbw_driver_all_channels_switch(&dev, 1);
-  rgbw_driver_set_color(&dev, 17, 28, 49, 251);
-  rgbw_driver_all_channels_switch(&dev, 0);
+  i2chw_error_t error = rgbw_driver_init(&dev, I2CHW_BUS_I2C0);
+  if (error != I2CHW_SUCCESS){
+    return error;
+  }
+  error = rgbw_driver_channel_switch(&dev, 2, 1);
+  if (error != I2CHW_SUCCESS){
+    return error;
+  }
+  error = rgbw_driver_set_channel_brightness(&dev, 2, 168);
+  if (error != I2CHW_SUCCESS){
+    return error;
+  }
+  error = rgbw_driver_all_channels_switch(&dev, 1);
+  if (error != I2CHW_SUCCESS){
+    return error;
+  }
+  error = rgbw_driver_set_color(&dev, 17, 28, 49, 251);
+  if (error != I2CHW_SUCCESS){
+    return error;
+  }
+  error = rgbw_driver_all_channels_switch(&dev, 0);
+  if (error != I2CHW_SUCCESS){
+    return error;
+  }
 
   return 0;
 }
